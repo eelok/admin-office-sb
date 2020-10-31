fc9be46a8444d91aaa5ecf57fc631f5ec2196c12
eelok <eelok@mail.ru>
2020-10-31 22:02:19 +0100

    add slide menu on icon click

--- src/App.js
+++ src/App.js
@@ -1,19 +1,28 @@
-import './App.css';
+import React, {useState} from "react";
 import {Route, Switch} from 'react-router-dom';
 import CreateConcert from "./components/create-concert/create-concert-component";
 import AllConcerts from "./pages/all-concerts/all-concerts-page.component";
 import EditConcert from "./pages/edit-concerts/edit-concerts-page.component";
 import Navigation from "./components/navigation/navigation-component";
+import './App.css';
 
 function App() {
+    let [menuOpen, setMenuOpen] = useState(false);
+
     return (
         <div className="App">
-            <Navigation/>
-            <Switch>
-                <Route exact={true} path='/addconcert' component={CreateConcert}/>
-                <Route exact={true} path='/concerts' component={AllConcerts}/>
-                <Route exact={true} path='/concerts/:id' component={EditConcert}/>
-            </Switch>
+            <div className={`overlay ${menuOpen ? 'overlay--open' : ''}`}/>
+            <Navigation
+                onMenuOpen={() => setMenuOpen(true)}
+                onMenuClose={() => setMenuOpen(false)}
+            />
+            <div className={`main ${menuOpen ? 'main--open' : ''}`}>
+                <Switch>
+                    <Route exact={true} path='/' component={AllConcerts}/>
+                    <Route exact={true} path='/addconcert' component={CreateConcert}/>
+                    <Route exact={true} path='/concerts/:id' component={EditConcert}/>
+                </Switch>
+            </div>
         </div>
     );
 }
