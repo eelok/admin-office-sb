e515e7b24c5c8c2f89a2f427e3de9eaafd5ca7bf
eelok <eelok@mail.ru>
2020-11-03 10:21:30 +0100

    change buttons

--- src/pages/all-concerts/all-concerts-page.component.jsx
+++ src/pages/all-concerts/all-concerts-page.component.jsx
@@ -3,7 +3,6 @@ import {db} from '../../firebase.js';
 import './all-concerts-style.scss'
 import Moment from "react-moment";
 import {ReactComponent as AddIcon} from "../../assets/plus.svg";
-import {ReactComponent as DeleteIcon} from "../../assets/cancel.svg";
 
 class AllConcerts extends React.Component {
     constructor(props) {
@@ -52,8 +51,8 @@ class AllConcerts extends React.Component {
             <div className='page-wrapper'>
                 <header className="concerts-header">
                     <h2 className='main-title'>Concerts:</h2>
-                    <div className="add-concert">
-                        <AddIcon onClick={this.onAddConcert}/>
+                    <div className="button-control add-concert" onClick={this.onAddConcert}>
+                        Add
                     </div>
                 </header>
                 <section className="event">
@@ -61,14 +60,20 @@ class AllConcerts extends React.Component {
                         concerts.map(item => (
                             <section key={item.id} className="event__container">
 
-                                <h3 className='event__title' onClick={() => this.onEdit(item.id)} >{item.title}</h3>
-                                <p className='event__description' onClick={() => this.onEdit(item.id)}>{item.description}</p>
-                                <div className="event__dt" onClick={() => this.onEdit(item.id)}><Moment className="event__day-time"
-                                                                   format="D MMMM yyyy, HH:mm">{item.startDate.toDate()}</Moment>
+                                <h3 className='event__title'  >{item.title}</h3>
+                                <p className='event__description'>{item.description}</p>
+                                <div className="event__dt">
+                                    <Moment className="event__day-time"
+                                           format="D MMMM yyyy, HH:mm">{item.startDate.toDate()}</Moment>
                                 </div>
-                                <p className='event__address' onClick={() => this.onEdit(item.id)}>{item.address}</p>
-                                <div onClick={() => this.onDelete(item.id)}
-                                        className="button-delete"><DeleteIcon/>
+                                <p className='event__address'>{item.address}</p>
+                                <div className='event__control'>
+                                <button className="button-control" onClick={() => this.onDelete(item.id)}>
+                                    Delete
+                                </button>
+                                <button className="button-control" onClick={() => this.onEdit(item.id)}>
+                                    Edit
+                                </button>
                                 </div>
                             </section>
                         ))
