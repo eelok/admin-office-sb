07e436883987a35f257eee10bea4207695593310
eelok <eelok@mail.ru>
2020-11-10 21:34:10 +0100

    change login class component to functional component

--- src/pages/login/login-page-component.jsx
+++ src/pages/login/login-page-component.jsx
@@ -1,83 +1,62 @@
-import React from "react";
+import React, {useState} from "react";
 import InputComponent from "../../components/Input/Input-component";
 import './login-style.scss'
 import {auth} from "../../firebase.js";
 
-export class Login extends React.Component {
+const Login = () => {
 
-    constructor(props) {
-        super(props);
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [isError, setIsError] = useState(false);
 
-        this.state = {
-            email: '',
-            password: '',
-            authorisationIsFail: false,
-        }
-    }
-
-
-    handleAuthentication = async event => {
-        event.preventDefault()
-        let {email, password, authorisationIsFail} = this.state;
+    const handleAuthentication = async event => {
+        event.preventDefault();
         try {
             await auth.signInWithEmailAndPassword(email, password);
-            this.setState({email: '', password: '', authorisationIsFail: false})
         } catch (error) {
-            this.setState({
-                ...this.state,
-                authorisationIsFail: true
-            })
+            setIsError(true);
         }
     }
 
-    handleChange = (event) => {
-        let {name, value} = event.target;
-        this.setState({[name]: value})
-    }
-
-    render() {
-        const {email, password, authorisationIsFail} = this.state;
-        return (
-            <div className='login'>
-                <form className='login__wrapper' onSubmit={this.handleAuthentication}>
-                    <div className='login__header'>
-                        <h2 className='login__title'>LOGIN</h2>
+    return (
+        <div className='login'>
+            <form className='login__wrapper' onSubmit={handleAuthentication}>
+                <div className='login__header'>
+                    <h2 className='login__title'>LOGIN</h2>
+                </div>
+                <section className='login__input-box'>
+                    {
+                        isError ?
+                            (<div className='login__notification'>
+                                <p>Введен неверно логин или параль</p>
+                            </div>)
+                            :
+                            null
+                    }
+                    <InputComponent
+                        type='email'
+                        label='Email'
+                        id='email'
+                        name='email'
+                        required={true}
+                        onChange={e => setEmail(e.target.value)}
+                    />
+                    <InputComponent
+                        type='password'
+                        label='Password'
+                        id="password"
+                        name='password'
+                        required={true}
+                        onChange={e => setPassword(e.target.value)}
+                    />
+                    <div className="login__btn-wrapper">
+                        <button className='btn-big'>Login</button>
                     </div>
-                    <section className='login__input-box'>
-                        {
-                            authorisationIsFail ?
-                                (<div className='login__notification'>
-                                    <p>Введен неверно логин или параль</p>
-                                </div>)
-                                :
-                                null
-                        }
-                        <InputComponent
-                            type='email'
-                            label='Email'
-                            id='email'
-                            name='email'
-                            required={true}
-                            value={email}
-                            onChange={this.handleChange}
-                        />
-                        <InputComponent
-                            type='password'
-                            label='Password'
-                            id="password"
-                            name='password'
-                            required={true}
-                            value={password}
-                            onChange={this.handleChange}
-                        />
-                        <div className="login__btn-wrapper">
-                            <button className='btn-big'>Login</button>
-                        </div>
-                    </section>
-                </form>
-            </div>
-        )
-    }
+                </section>
+            </form>
+        </div>
+    )
+
 }
 
 export default Login;
